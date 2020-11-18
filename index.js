758ca6b28ff8f45ded74c5d65465a609254065e0
Armand72 <armandmeunier@hotmail.fr>
2020-11-18 14:48:14 +0100

    changed heroku config

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ io.on('connect', (socket) => {
 
 // Heroku deployment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, './frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
   });
 }
 
