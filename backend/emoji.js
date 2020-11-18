e20d67ed2387a3af67320bb0e7b27c2a85f9dc5d
Armand72 <armandmeunier@hotmail.fr>
2020-11-18 11:54:05 +0100

    fixed issues with sockets, new data structure

--- backend/emoji.js
+++ backend/emoji.js
@@ -1,35 +1,32 @@
-const emojis = 
-[
-  {
-      id:1,
-      counter: 0,
-  },
-  {
-    id:2,
-    counter: 0,
-},
-{
-    id:3,
-    counter: 0,
-},
-{
-    id:4,
-    counter: 0,
-},
-{
-    id:5,
-    counter: 0,
-},
-];
+// const emojis = 
+// [
+//   {
+//       id:1,
+//       counter: 0,
+//   },
+//   {
+//     id:2,
+//     counter: 0,
+// },
+// {
+//     id:3,
+//     counter: 0,
+// },
+// {
+//     id:4,
+//     counter: 0,
+// },
+// {
+//     id:5,
+//     counter: 0,
+// },
+// ];
 
-const incrementEmojiCounter = (idEmoji) => {
-    const index = emojis.indexOf(emoji => emoji.id === idEmoji)
+// const incrementEmojiCounter = (idEmoji) => {
+//     emojis[idEmoji- 1].counter++
+//     return emojis[idEmoji - 1]
+// };
 
-    emojis[index].counter++
-  
-    return emojis[index]
-};
-
-module.exports = {
-    incrementEmojiCounter
-}
+// module.exports = {
+//     incrementEmojiCounter
+// }
