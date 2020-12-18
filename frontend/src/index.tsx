08b6a343bc4b0de75a26a25c2b62a4e8aae08c4e
mathildetho <mathilde.thoraninth@gmail.com>
2020-12-18 10:52:26 +0100

    add client graphql

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri:'http://localhost:8000/',
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
