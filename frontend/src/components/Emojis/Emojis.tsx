11375877a4617e182f3ad6a62af9b3dd32309fea
lauren <laurenzolivier@gmail.com>
2021-01-07 12:19:06 +0100

    Add teacher student and emojis files

--- frontend/src/components/Emojis/Emojis.tsx
+++ frontend/src/components/Emojis/Emojis.tsx
@@ -1,46 +1,54 @@
 import React from 'react';
 import { Emoji, emojis } from '../../datas/Emojis';
-import { MoodCounter } from '../Teacher/Teacher';
+import { MoodCounter } from '../../datas/Emojis';
 
 interface EmojisProps {
-    handleClick?: (name: string, category:string) => void;
-    isStudent?: boolean,
-    emojisCounts?: MoodCounter
+  handleClick?: (name: string, category: string) => void;
+  isStudent?: boolean;
+  emojisCounts?: MoodCounter;
 }
 
-const Emojis: React.FC<EmojisProps> = ({handleClick, isStudent, emojisCounts}) => {
-    return (
-        <div className={`${isStudent ? "student_emojis" : "teacher_emojis"}`}>
-            {emojis &&
-              emojis.map((emoji: Emoji) => (
-                  <div key={emoji.name}>
-                    {isStudent ? (
-                        <button
-                            key={emoji.name}
-                            className="student_emoji_button"
-                            type="button"
-                            onClick={() => handleClick && handleClick(emoji.name, emoji.category)}
-                        >
-                            <img
-                                className="student_emoji_img"
-                                src={emoji.image}
-                                alt={emoji.name}
-                            />
-                        </button>
-                    ) : (
-                        <div key={emoji.name} className="teacher_emoji_headband">
-                            <img
-                                className="teacher_emoji_img"
-                                src={emoji.image}
-                                alt={emoji.name}
-                            />
-                            <p role="emojiCount">{emojisCounts && emojisCounts[emoji.name]}</p>
-                        </div>
-                    )}
-                </ div>
-              ))}
+const Emojis: React.FC<EmojisProps> = ({
+  handleClick,
+  isStudent,
+  emojisCounts,
+}) => {
+  return (
+    <div className={`${isStudent ? 'student_emojis' : 'teacher_emojis'}`}>
+      {emojis &&
+        emojis.map((emoji: Emoji) => (
+          <div key={emoji.name}>
+            {isStudent ? (
+              <button
+                key={emoji.name}
+                className="student_emoji_button"
+                type="button"
+                onClick={() =>
+                  handleClick && handleClick(emoji.name, emoji.category)
+                }
+              >
+                <img
+                  className="student_emoji_img"
+                  src={emoji.image}
+                  alt={emoji.name}
+                />
+              </button>
+            ) : (
+              <div key={emoji.name} className="teacher_emoji_headband">
+                <img
+                  className="teacher_emoji_img"
+                  src={emoji.image}
+                  alt={emoji.name}
+                />
+                <p role="emojiCount">
+                  {emojisCounts && emojisCounts[emoji.name]}
+                </p>
+              </div>
+            )}
           </div>
-    )
-}
+        ))}
+    </div>
+  );
+};
 
 export default Emojis;
--- frontend/src/components/Teacher/Teacher.tsx
+++ frontend/src/components/Teacher/Teacher.tsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
-import socket from '../../socket/socket'
-import Emojis from '../Emojis/Emojis'
+import React, { useState, useEffect } from 'react';
+import socket from '../../socket/socket';
+import Emojis from '../Emojis/Emojis';
 import './Teacher.css';
-
-export interface MoodCounter {
-  [k: string]: number;
-}
+import { getColorByMood } from '../Methods/getColorByMood';
+import { MoodCounter } from '../../datas/Emojis';
 
 const Teacher = (): JSX.Element => {
   const [emojisCounts, setEmojisCounts] = useState<MoodCounter>({
@@ -18,46 +16,15 @@ const Teacher = (): JSX.Element => {
   });
   const [totalStudents, setTotalStudents] = useState(0);
 
-  const handleConnection = (): void => {
+  useEffect(() => {
+    socket.emit('joinTeacher', {});
+    socket.on('sendUserCount', (userCount: number) => {
+      setTotalStudents(userCount);
+    });
     socket.on('getIncrement', (moodCounter: MoodCounter) => {
       setEmojisCounts(moodCounter);
     });
-  };
-
-  useEffect(() => {
-    socket.emit('joinTeacher', {});
-  });
-
-  useEffect(() => {
-    socket.on('joinTeacher', (moodCounter: MoodCounter) => {
-      setEmojisCounts(moodCounter);
-    });
-
-    socket.on('sendUserCount', (userCount: number) => {
-      console.log(userCount);
-      setTotalStudents(userCount);
-    });
   }, []);
-
-  const getColor = (): string => {
-    const unhappys: number = emojisCounts.Thinking + emojisCounts.Dead;
-    const unhappysPercentage: number = (unhappys / totalStudents) * 100;
-
-    if (unhappysPercentage >= 20 && unhappysPercentage < 50) {
-      return 'rgba(255, 249, 61, 0.7)';
-    }
-    if (unhappysPercentage >= 50 && unhappysPercentage < 80) {
-      return 'rgba(230, 147, 5, 0.7)';
-    }
-    if (unhappysPercentage >= 80) {
-      return 'rgba(235, 0, 0, 0.7)';
-    }
-    return 'rgba(148, 234, 72, 0.7)';
-  };
-
-  useEffect(() => {
-    handleConnection();
-  }, [emojisCounts]);
 
   return (
     <div className="teacher">
@@ -66,11 +33,11 @@ const Teacher = (): JSX.Element => {
         role="heading"
         aria-level={2}
         className="teacher_vertical_panel"
-        style={{ backgroundColor: getColor() }}
+        style={{ backgroundColor: getColorByMood(emojisCounts, totalStudents) }}
       >
         <div className="teacher_emojis_container">
           <div className="teacher_emojis">
-            <Emojis emojisCounts={emojisCounts} /> 
+            <Emojis emojisCounts={emojisCounts} />
           </div>
           <p>student length: {totalStudents}</p>
         </div>
