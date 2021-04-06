825c80c1ed88fc1c157b2fa5a846e9981788bc72
Amandine LEFEBVRE <57723313+amandine-dev@users.noreply.github.com>
2021-04-06 15:49:33 +0200

    fix eslint pb + error with data in signupform

--- frontend/src/graphql/queries/getUser.ts
+++ frontend/src/graphql/queries/getUser.ts
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+// eslint-disable-next-line import/prefer-default-export
 export const FIND_USER = gql`
   query($userId: String!) {
     getUser(_id: $userId) {
