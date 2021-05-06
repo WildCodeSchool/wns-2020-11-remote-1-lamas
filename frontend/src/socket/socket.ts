786937cde2c2d07bff8bb5a09e35b031bca0be1d
Amandine LEFEBVRE <57723313+amandine-dev@users.noreply.github.com>
2021-05-06 10:40:02 +0200

    change env and port

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,10 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const ENDPOINT =
-  process.env.NODE_ENV === 'production'
-    ? `${process.env.REACT_APP_LAMAS_BACK}`
-    : `${process.env.REACT_APP_LOCALHOST}`;
-
+const ENDPOINT = `${process.env.REACT_APP_LAMAS_BACK}`;
 const socket: Socket = io(ENDPOINT, {
   transports: ['websocket'],
 });
