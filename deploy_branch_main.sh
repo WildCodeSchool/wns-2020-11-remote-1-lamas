395694d88b36c356fea7f2aa447739090cf47045
Armand Meunier <armandmeunier@hotmail.fr>
2021-06-03 16:30:28 +0200

    Update deploy_branch_main.sh

--- deploy_branch_main.sh
+++ deploy_branch_main.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/main && git clean -f -d
-GATEWAY_PORT=$PORT docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d
+GATEWAY_PORT=$PORT docker-compose -f docker-compose.yml -f docker-compose.staging.yml up --build -d
