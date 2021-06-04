4e4437e9b5cda60e3c0f6a74dcbb5905ba745eed
Stéphane <contact.xsprod@gmail.com>
2021-06-04 11:04:54 +0200

    test

--- frontend/src/graphql/queries/getUser.ts
+++ frontend/src/graphql/queries/getUser.ts
@@ -1,9 +1,5 @@
 import { gql } from '@apollo/client';
 
-<<<<<<< HEAD
-=======
-// eslint-disable-next-line
->>>>>>> 4092e4205357b1bd86ffb7b022b3524aa03ed16d
 export const FIND_USER = gql`
   query($userId: ID!) {
     getUser(_id: $userId) {
